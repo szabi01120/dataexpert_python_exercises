4174d18eaba28a5ff76c8d22c487694f1c358cfe
Hajnal Szabolcs <szabolcs.hajnal@dataexpert.hu>
2024-09-06 16:38:04 +0200

    pandas google play analysis

---
+++ pandas-data-analysis/README.md
@@ -0,0 +1,75 @@
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/schlende/practical-pandas-projects/master)
+
+# Practical pandas projects
+
+[pandas](https://pandas.pydata.org/) is a python library for doing exploratory data analysis.
+
+There are [great docs](https://pandas.pydata.org/pandas-docs/stable/) and [lots of online tutorials](https://pandas.pydata.org/pandas-docs/stable/getting_started/tutorials.html) teaching the basics, but I've seen a lot of people asking what they can work on after they've gone through the tutorials.
+
+My aim here is to create a list of project ideas that are exciting and practical.
+
+Most projects include:
+
+* An explanation of why the dataset is interesting
+* Ideas for questions the data can answer
+* A bit of sample data you can play with (in some cases)
+* Instructions for how to get data
+
+## The project ideas
+
+### Market research
+
+* [Figure out which products sell best on Etsy](notebooks/etsy-listing-dataset.ipynb)
+* [Figure out what apps to build using Google Play data](notebooks/google-play-apps.ipynb)
+* [Find products to sell on Amazon](notebooks/amazon-products.ipynb)
+* Find freelance opportunities using Fiverr data
+* Search for products you could build on Creative Market
+
+### Website metrics
+
+* [Track your business KPIs with your webserver's log data](notebooks/webserver-log-data.ipynb)
+
+### Government data
+
+* [Understanding global market trends with import / export data](notebooks/usgov-import-export.ipynb)
+
+### Content marketing
+
+* Munge data to create sharable infographics
+* Figure out what kind of content gets shared on Facebook
+
+### Measuring Yourself
+
+* Tracking your fitness
+* Tracking your productivity
+* Tracking your phone usage
+
+# How to use the sample data
+
+The easiest way to is to use run the notebooks online using Binder. [Click this link](https://mybinder.org/v2/gh/schlende/practical-pandas-projects/master) and wait for Binder to load. You'll be able to start exploring the data without having to install any dependencies on your computer.
+
+Want to run the notebooks yourself on your computer?
+
+You'll need iPython notebook and pandas.
+
+Open a terminal and run:
+
+    git clone https://github.com/schlende/practical-pandas-projects.git
+
+    pip install jupyter
+    pip install -r requirements.txt
+
+    cd practical-pandas-projects
+    jupyter notebook
+
+
+A tab should open in your browser at ```http://localhost:8888```
+
+
+# Contribute
+Have suggestions? Want to contribute your own ideas? Send me an email or make a pull request!
+
+
+# License
+
+MIT
---
+++ pandas-data-analysis/notebooks/app.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import urllib.robotparser
+
+def crawlSite(site_url):
+    rp = urllib.robotparser.RobotFileParser()
+    rp.set_url("https://www.etsy.com/robots.txt")
+    rp.read()
+    rp.crawl_delay("*")
+
+    test_crawl_url = site_url
+
+    can_crawl_listings = rp.can_fetch("*", test_crawl_url)
+    return print("We can crawl? {0}\n".format(can_crawl_listings))
+
+def etsyListing():
+    df = pd.read_csv('../datasets/etsy-mens-t-shirts-11-15-2017.csv')
+    etsyUrl = "https://www.etsy.com/listing/478395857/big-little-shirt-big-little-sorority"
+    
+    # most traffic, views on tshirt
+    max_views_product = df.set_index('seller_name')['views'].idxmax()
+    max_views = df.set_index('product_title')['views'].max()
+    print("Most views:", max_views_product, "with", max_views, "views\n")
+    
+    # describe the data
+    print("Etsy describe:\n", df.describe().round(3))
+
+    # check if we can crawl etsy
+    crawlSite(etsyUrl)
+    
+def playStoreListing():
+    df = pd.read_csv('../datasets/google-play-store-11-2018.csv')
+    gpUrl = "https://play.google.com/store/apps/details?id=com.wildnotion.poetscorner"
+    
+    print("Google play describe:\n", df.describe().round(3))
+    
+    print("Most downloadad categories:")
+    most_downloaded = df.groupby('genre')['min_installs'].sum().head(5)
+    print(most_downloaded, "\n")
+    
+    print("Least downloadad categories: ")
+    least_downloaded = df.groupby('genre')['min_installs'].sum()
+    print(least_downloaded.sort_values(ascending=True).head(5), "\n")
+    
+    # check if we can crawl google play
+    crawlSite(gpUrl)
+    
+    # offers iap and is free
+    print("Offers in-app purchases and is free:")
+    result_df = df[(df['offers_iap'] == True) & (df['price'] == 0)]
+    print(result_df[['title', 'price', 'offers_iap']])
+    
+    # avg rating of apps that offer iap
+    avg_rating = df[df['offers_iap'] == True]['score'].mean().round(3)
+    print("\nAverage rating of apps that offer in-app purchases:", avg_rating)
+    
+    #app with lowest rating
+    lowest_rating_name = df[df['score'] > 0].set_index('title')['score'].idxmin()
+    lowest_rating = df[df['score'] > 0].set_index('title')['score'].min()
+    print("\nLowest rating:", lowest_rating_name, "with", lowest_rating, "rating")
+    
+    # star rating percentage comparison by genre
+    star_ratings = df.groupby('genre')[['rating_one_star', 'rating_two_star', 'rating_three_star', 'rating_four_star', 'rating_five_star']].sum()
+    star_ratings_percentage = star_ratings.div(star_ratings.sum(axis=1), axis=0).mul(100).round(2)
+    print("\nStar rating percentage comparison by genre:\n", star_ratings_percentage.head(5))
+
+def main():
+    etsyListing()
+    playStoreListing()
+    
+if __name__ == '__main__':
+    main()
